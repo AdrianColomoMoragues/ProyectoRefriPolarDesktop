3c80403c94c3278362e03314b5c2564faeeb9e34
Adrián Colomo <adricomo94@gmail.com>
2024-06-18 12:18:48 +0200

    Update Cliente.cs

--- ProyectoRefriPolar/Net/Cliente.cs
+++ ProyectoRefriPolar/Net/Cliente.cs
@@ -21,39 +21,28 @@ namespace ProyectoRefriPolar.Net
 
             try
             {
-                // Connect to a Remote server
-                // Get Host IP Address that is used to establish a connection
-                // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-                // If a host has multiple addresses, you will get a list of addresses
                 IPHostEntry host = Dns.GetHostEntry("localhost");
                 IPAddress ipAddress = host.AddressList[0];
                 IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
 
-                // Create a TCP/IP  socket.
                 Socket sender = new Socket(ipAddress.AddressFamily,
                     SocketType.Stream, ProtocolType.Tcp);
 
-                // Connect the socket to the remote endpoint. Catch any errors.
                 try
                 {
-                    // Connect to Remote EndPoint
                     sender.Connect(remoteEP);
 
                     Console.WriteLine("Socket connected to {0}",
                         sender.RemoteEndPoint.ToString());
 
-                    // Encode the data string into a byte array.
                     byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
 
-                    // Send the data through the socket.
                     int bytesSent = sender.Send(msg);
 
-                    // Receive the response from the remote device.
                     int bytesRec = sender.Receive(bytes);
                     Console.WriteLine("Echoed test = {0}",
                         Encoding.ASCII.GetString(bytes, 0, bytesRec));
 
-                    // Release the socket.
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
 
